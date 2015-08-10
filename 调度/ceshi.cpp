5c2cab84f1f685b78b98d2a0017503f116769498
Blair1993 <469317300@qq.com>
2015-08-10 14:01:01 +0800

    调度

---
+++ 调度/ceshi.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include"begin.h"
+using namespace std;
+int main()
+{
+	input();
+	return 0;
+}
---
+++ 调度/work.h
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class work
+{
+public:
+	string name;
+	int si;//进入时间
+	int ti;//优先数
+	int time;//长度
+};
